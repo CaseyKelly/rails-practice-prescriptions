42dec3e2a3d33d786b026882063b327ea892c129
CaseyKelly <casey.kelly@colorado.edu>
2015-04-16 17:55:59 -0600

    users can see a patient's list of prescriptions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "dashboard#index"
+
+  resources :patients
+
   get "signin" => "sessions#new", as: :signin
   post "signin" => "sessions#create"
   delete "signout" => "sessions#destroy", as: :signout
