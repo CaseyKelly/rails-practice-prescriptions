020fc00f9f94007b7d205c20c3d7c8120a02c27c
CaseyKelly <casey.kelly@colorado.edu>
2015-04-16 19:49:38 -0600

    add custom validator for dosage must start with a number

--- app/models/perscription.rb
+++ app/models/perscription.rb
@@ -5,5 +5,12 @@ class Perscription < ActiveRecord::Base
   belongs_to :medication
 
   validates_presence_of :medication_id, :dosage, :schedule, :starts_on, :ends_on, :user_id, :patient_id
+  validate :dosage_starts_with_number
+
+  def dosage_starts_with_number
+    if dosage.present? && dosage[0].class != Fixnum
+      errors.add(:dosage, "must start with a number")
+    end
+  end
 
 end
