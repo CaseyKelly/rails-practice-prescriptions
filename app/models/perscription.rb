d3fc48361d746deae338b71f1428295a9a652b25
CaseyKelly <casey.kelly@colorado.edu>
2015-04-16 20:03:24 -0600

    end date cannot be before start date on perscriptions

--- app/models/perscription.rb
+++ app/models/perscription.rb
@@ -5,11 +5,17 @@ class Perscription < ActiveRecord::Base
   belongs_to :medication
 
   validates_presence_of :medication_id, :dosage, :schedule, :starts_on, :ends_on, :user_id, :patient_id
-  validate :dosage_starts_with_number
+  validate :dosage_starts_with_number, :end_date_is_after_start_date
 
   def dosage_starts_with_number
-    if dosage.present? && dosage[0].class != Fixnum
+    if dosage[0].to_i.to_s != dosage[0]
       errors.add(:dosage, "must start with a number")
+    end
+  end
+
+  def end_date_is_after_start_date
+    if ends_on < starts_on
+      errors.add(:end_date, "must be after the start date")
     end
   end
 
